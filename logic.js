074dcff7a87d2dc088277f4a1b87a05634e3cba3
isabellawidler <[isabella.widler@datenkraftwek.at]>
2024-08-14 08:22:32 +0200

    Add button to call function changeDivNumber and make grid fit into container

--- logic.js
+++ logic.js
@@ -1,31 +1,55 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("gridDiv");
-    container.appendChild(gridDiv);
-    for (let i = 0; i < 16; i++) {
-        const gridDiv = document.createElement("div");
-        gridDiv.classList.add("gridDiv");
-        container.appendChild(gridDiv);
+//let numberGrids = window.prompt("Enter the grid size (max 100)");
+let numberInput = document.querySelector(".input");
+let buttonSubmit = document.querySelector(".submit");
+
+// TODO create function to make grid with numberDivs
+
+function changeDivNumber() {
+    container.innerHTML = "";
+    let numberDivs = numberInput.value;
+    if (numberDivs <= 0 || numberDivs > 100) {
+        alert("Please choose a size between 1 and 100");
+    }
+    else {
+        let divSize = 800 / numberDivs;
+        for (let i = 0; i < numberDivs; i++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.style.width = `${divSize}px`;
+            rowDiv.style.height = `${divSize}px`;
+            rowDiv.classList.add("gridDiv");
+            container.appendChild(rowDiv);
+            
+            for (let y = 0; y < numberDivs; y++) {
+                const columnDiv = document.createElement("div");
+                columnDiv.style.width = `${divSize}px`;
+                columnDiv.style.height = `${divSize}px`;
+                columnDiv.classList.add("gridDiv");
+                container.appendChild(columnDiv);
+            }   
+        }
+        const gridDivs = document.querySelectorAll(".gridDiv");
+
+        gridDivs.forEach(div => {
+            div.style.filter = ("brightness(1.0)");
+            div.dataset.brightness = "1.0";
+        });
+
+        function handleGridColor(event) {
+            let currentDiv = event.target;
+            let currentBrightness = parseFloat(currentDiv.dataset.brightness);
+            let newBrightness = Math.max(currentBrightness - 0.1, 0);
+            currentDiv.style.filter = `brightness(${newBrightness})`;
+            currentDiv.dataset.brightness = newBrightness.toString();
+        }
+
+        gridDivs.forEach(div => {
+            div.addEventListener("mouseover", handleGridColor);
+        });
     }
 }
 
-const gridDivs = document.querySelectorAll(".gridDiv");
+buttonSubmit.addEventListener("click", changeDivNumber);
 
-gridDivs.forEach(div => {
-    div.style.filter = ("brightness(1.0)");
-    div.dataset.brightness = "1.0";
-});
 
-function handleGridColor(event) {
-    let currentDiv = event.target;
-    let currentBrightness = parseFloat(currentDiv.dataset.brightness);
-    let newBrightness = Math.max(currentBrightness - 0.1, 0);
-    currentDiv.style.filter = `brightness(${newBrightness})`;
-    currentDiv.dataset.brightness = newBrightness.toString();
-}
-
-gridDivs.forEach(div => {
-    div.addEventListener("mouseover", handleGridColor);
-});
